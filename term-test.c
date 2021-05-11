29afce383ffa8d4c97bf15e4e6fb83a63644a191
Aniakuos <soukaina.sghuri@gmail.com>
2021-05-11 14:24:11 +0000

    after norm

--- term-test.c
+++ term-test.c
@@ -44,42 +44,6 @@ char	*ft_strdup(const char *s1)
 	p[j] = '\0';
 	return (p);
 }
-/* Given a reference (pointer to pointer) to the head
-   of a DLL and an int, appends a new node at the end  */
-// void append(t_sdlist **head_ref, char *new_data)
-// {
-//     /* 1. allocate node */
-//     t_sdlist *new_node = (t_sdlist*)malloc(sizeof(t_sdlist));
- 
-//     t_sdlist *last = *head_ref; /* used in step 5*/
- 
-//     /* 2. put in the data  */
-//     new_node->line = ft_strdup(new_data);
- 
-//     /* 3. This new node is going to be the last node, so
-//           make next of it as NULL*/
-//     new_node->next = NULL;
- 
-//     /* 4. If the Linked List is empty, then make the new
-//           node as head */
-//     if (*head_ref == NULL) {
-//         new_node->prev = NULL;
-//         *head_ref = new_node;
-//         return;
-//     }
- 
-//     /* 5. Else traverse till the last node */
-//     while (last->next != NULL)
-//         last = last->next;
- 
-//     /* 6. Change the next of last node */
-//     last->next = new_node;
- 
-//     /* 7. Make last node as previous of new node */
-//     new_node->prev = last;
- 
-//     return;
-// }
 
 void	ft_putstr(char *s)
 {
@@ -95,39 +59,22 @@ void	ft_putstr(char *s)
 	}
 }
 
-void printList(t_sdlist *node)
+void ft_printlist_hist(t_sdlist *node)
 {
-    //t_sdlist *last;
-    //printf("\nTraversal in forward direction \n");
     while (node != NULL) {
 		ft_putstr("------\n");
 		ft_putstr("Prev : ");
 		if(node->prev)
-		{
 			ft_putstr(node->prev->line);
-		}
 		ft_putstr(" | ");
 		ft_putstr("line : ");
 		ft_putstr(node->line);
-		//ft_putstr("\n");
 		ft_putstr(" | Next : ");
 		if(node->next)
-		{
-			ft_putstr(node->next->line);	
-		}
-			
-		//ft_putstr("\n");
-		
+			ft_putstr(node->next->line);
 		ft_putstr("\n");
         node = node->next;
     }
- 
-    // printf("\n--- \n");
-    // while (last != NULL) {
-	// 	//ft_putstr(last->line);
-    //     //printf("|%s|", last->line);
-    //     last = last->prev;
-    // }
 }
 
 int ft_strlen(char *s)
@@ -137,8 +84,6 @@ int ft_strlen(char *s)
 		i++;
 	return (i);
 }
-
-
 
 int	nbr_length(int n)
 {
@@ -190,29 +135,28 @@ int		putchar_tc(int tc)
 	return (0);
 }
 
-void	move_cursor_left(int *col, int *row, char *cm)
+void	move_cursor_left(int *col)
 {
 	if (*col == 0)
 		return ;
 	--(*col);
-	tputs(tgoto(cm, *col, *row), 1, putchar_tc);
-
+	//tputs(tgoto(cm, *col, *row), 1, putchar_tc);
+	write(1, tgetstr("le", NULL), ft_strlen(tgetstr("le", NULL)));
 }
 
-void	move_cursor_right(int *col, int *row, char *cm)
+void	move_cursor_right(int *col)
 {
 	++(*col);
-	tputs(tgoto(cm, *col, *row), 1, putchar_tc);
-
+	//tputs(tgoto(cm, *col, *row), 1, putchar_tc);
+	write(1, tgetstr("nd", NULL), ft_strlen(tgetstr("nd", NULL)));
 }
 
-void	delete_end(int *col, int *row, char *cm, char *ce)
+void	delete_end(int *col)
 {
 	if (*col != 0)
 		--(*col);
-	tputs(tgoto(cm, *col, *row), 1, putchar_tc);
-	tputs(ce, 1, putchar_tc);
-	//write(1, tgetstr("ce", NULL), ft_strlen(tgetstr("ce", NULL)));
+	write(1, tgetstr("le", NULL), ft_strlen(tgetstr("le", NULL)));
+	write(1, tgetstr("ce", NULL), ft_strlen(tgetstr("ce", NULL)));
 	tab[ft_strlen(tab) - 1] = '\0';
 }
 
@@ -289,61 +233,47 @@ t_sdlist	*ft_lstnew(void *line)
 	lst->next = NULL;
 	return (lst);
 }
-void insertAfter(t_sdlist *prev_node, char *new_data)
+void ft_insertafter(t_sdlist *prev_node, char *new_data)
 {
-	
-    /*1. check if the given prev_node is NULL */
-    // if (prev_node == NULL) {
-    //     printf("the given previous node cannot be NULL");
-    //     return;
-    // }
- 
     /* 2. allocate new node */
     t_sdlist *new_node = (t_sdlist*)malloc(sizeof(t_sdlist));
- 
     /* 3. put in the data  */
     new_node->line = ft_strdup(new_data) ;
- 
     /* 4. Make next of new node as next of prev_node */
     new_node->next = prev_node->next;
- 
     /* 5. Make the next of prev_node as new_node */
     prev_node->next = new_node;
- 
     /* 6. Make prev_node as previous of new_node */
     new_node->prev = prev_node;
- 
     /* 7. Change previous of new_node's next node */
     //if (new_node->next != NULL)
         new_node->next->prev = new_node;
 }
 
-void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, 1);
-}
+// void	ft_putchar_fd(char c, int fd)
+// {
+// 	write(fd, &c, 1);
+// }
 
-void	ft_putnbr_fd(int n, int fd)
-{
-	unsigned int nb;
-
-	if (n < 0)
-	{
-		write(fd, "-", 1);
-		nb = n * (-1);
-	}
-	else
-		nb = n;
-	if (nb < 10)
-		ft_putchar_fd(nb + 48, fd);
-	else
-	{
-		ft_putnbr_fd(nb / 10, fd);
-		ft_putchar_fd(nb % 10 + 48, fd);
-	}
-}
-
-int		main(void)
+// void	ft_putnbr_fd(int n, int fd)
+// {
+// 	unsigned int nb;
+// 	if (n < 0)
+// 	{
+// 		write(fd, "-", 1);
+// 		nb = n * (-1);
+// 	}
+// 	else
+// 		nb = n;
+// 	if (nb < 10)
+// 		ft_putchar_fd(nb + 48, fd);
+// 	else
+// 	{
+// 		ft_putnbr_fd(nb / 10, fd);
+// 		ft_putchar_fd(nb % 10 + 48, fd);
+// 	}
+// }
+void	ft_ini_term()
 {
 	/* change term settings */
 	struct termios term;
@@ -353,12 +283,17 @@ int		main(void)
 	term.c_cc[VMIN] = 1;
 	term.c_cc[VTIME] = 0;
 	tcsetattr(STDIN_FILENO, TCSANOW, &term);
-
+	
+}
+int		main(void)
+{
+	
+	ft_ini_term();
 	/* init termcap */
 	tgetent(NULL, "xterm");
 	char *cm = tgetstr("cm", NULL); //cursor motion
-	char *ce = tgetstr("ce", NULL);
-	//write(1,tgetstr("ce", NULL), ft_strlen(tgetstr("ce", NULL))); //clear line from cursor
+	char *ce = tgetstr("ce", NULL);//clear line from cursor
+	//write(1,tgetstr("ce", NULL), ft_strlen(tgetstr("ce", NULL))); 
 	char *tmp;
 	int c = 0;
 	int row;
@@ -377,27 +312,33 @@ int		main(void)
 	{
 		get_cursor_position(&col, &row);
 		if (c == LEFT_ARROW)
-			move_cursor_left(&col, &row, cm);
+			move_cursor_left(&col);
 		else if (c == RIGHT_ARROW)
-			move_cursor_right(&col, &row, cm);
+			move_cursor_right(&col);
 		else if (c == BACKSPACE)
-			delete_end(&col, &row, cm, ce);
+			delete_end(&col);
 		else if(c == ENTER_ARROW)
 		{
+			
 			//ft_putnbr_fd(ft_lstsize(hist), 1);
 			if(ft_lstsize(hist) == 1) {
-				ft_lstadd_front(&hist, ft_lstnew(ft_strdup(tab)));
-				hist->prev = NULL;
-				ft_lstlast(hist)->prev = hist;
+				if(tab[0] != '\0')
+				{
+					ft_lstadd_front(&hist, ft_lstnew(ft_strdup(tab)));
+					hist->prev = NULL;
+					ft_lstlast(hist)->prev = hist;
+				}	
 			}
 			else if (ft_lstsize(hist) > 1){
-				insertAfter(ft_lstlast(hist)->prev, tab);
+				if(tab[0] != '\0')
+					ft_insertafter(ft_lstlast(hist)->prev, tab);	
 			}
 			//ft_putstr("\r");
 			free(tab);
-			printList(hist);
+			ft_printlist_hist(hist);
 			tab = ft_strdup("");
 			last = ft_lstlast(hist);
+			ft_putstr("\n");
 			//break;
 		}
 		else if (c == UP_ARROW)
